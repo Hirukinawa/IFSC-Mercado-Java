9779fd4f84f0861aea35051b7510db3b6a24d0af
João Pedro <joaopedrocode0@gmail.com>
2024-07-08 21:16:22 -0300

    v1.1 - sla

--- target/generated-sources/annotations/com/mycompany/mercadomaven_jpa_hibernate/model/bo/HistoricoMovimentacao_.java
+++ target/generated-sources/annotations/com/mycompany/mercadomaven_jpa_hibernate/model/bo/HistoricoMovimentacao_.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-07-08T21:00:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-07-08T21:10:11")
 @StaticMetamodel(HistoricoMovimentacao.class)
 public class HistoricoMovimentacao_ { 
 
--- target/generated-sources/annotations/com/mycompany/mercadomaven_jpa_hibernate/model/bo/Pagar_.java
+++ target/generated-sources/annotations/com/mycompany/mercadomaven_jpa_hibernate/model/bo/Pagar_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-07-08T21:00:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-07-08T21:10:11")
 @StaticMetamodel(Pagar.class)
 public class Pagar_ { 
 
